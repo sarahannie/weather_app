a74064dc1f3687dc85a45c6c506b6f66e849fb6b
sarahannie <sarahndianekwu@gmail.com>
2024-05-15 15:30:49 +0100

    adjusted the image

--- index.js
+++ index.js
@@ -17,7 +17,25 @@ const checkweather = async(city) => {
         document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
         document.querySelector(".wind").innerHTML = data.wind.speed + "km/h";
     
-        weatherIcon.src= `images/${(data.weather[0].main)}.png`;
+        // weatherIcon.src= `images/${(data.weather[0].main)}.png`;
+        if(data.weather[0].main == "Clouds"){
+            weatherIcon.src = "images/clouds.png";
+        }
+        else if(data.weather[0].main == "Clear"){
+            weatherIcon.src = "images/clear.png";
+        }
+        else if(data.weather[0].main == "Snow"){
+            weatherIcon.src = "images/snow.png";
+        }
+        else if(data.weather[0].main == "Rain"){
+            weatherIcon.src = "images/rain.png";
+        }
+        else if(data.weather[0].main == "Drizzle"){
+            weatherIcon.src = "images/drizzle.png";
+        }
+        else if(data.weather[0].main == "Mist"){
+            weatherIcon.src = "images/mist.png";
+        }
         document.querySelector(".error").style.display = "none";
         document.querySelector(".weather").style.display= "block";
     }
